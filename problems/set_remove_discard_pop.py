cde50eb0f3b05ee91db9d1cf543369ed02bc86bf
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-04 12:39:26 +0530

    set remove pop discard example problem

---
+++ problems/set_remove_discard_pop.py
@@ -0,0 +1,19 @@
+n = int(input())
+s = set(map(int, input().split()))
+
+m = int(input())
+
+for _ in range(m):
+    cmd = input().split()
+    
+    if cmd[0] == 'pop'.lower():
+        s.pop()
+        
+    elif cmd[0] == 'remove'.lower():
+        s.remove(int(cmd[1]))
+    
+    elif cmd[0] == 'discard'.lower():
+        s.discard(int(cmd[1]))
+        
+print(sum(s))
+    
