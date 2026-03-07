554e67fdbfe6846d291f8f4ae5249d7747e2e222
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-07 21:01:26 +0530

    set captains room problem added

---
+++ problems/captain_room.py
@@ -0,0 +1,15 @@
+# k = int(input())
+# room_numbers = list(map(int, input().split()))
+
+# for room in set(room_numbers):
+#     counting = room_numbers.count(room)
+
+#     if counting == 1:
+#         print(room)
+
+k = int(input())
+rooms = list(map(int, input().split()))
+
+captain = (sum(set(rooms)) * k - sum(rooms)) // (k - 1)
+
+print(captain)
