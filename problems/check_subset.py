924eb0f9131470390eb756063306535ca88db769
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-08 13:26:58 +0530

    other method and comment

--- problems/check_subset.py
+++ problems/check_subset.py
@@ -1,3 +1,5 @@
+# Check whether set A is a subset of set B
+
 t = int(input())
 
 for _ in range(t):
@@ -8,3 +10,21 @@ for _ in range(t):
     B = set(map(int, input().split()))
     
     print(A.issubset(B))
+
+     # or 
+'''
+t = int(input())
+
+for _ in range(t):
+    _ = int(input())  # size of A
+    A = set(map(int, input().split()))
+    
+    _ = int(input())  # size of B
+    B = set(map(int, input().split()))
+
+    if A.union(B) == B:
+        print(True)
+    else:
+        print(False)
+
+'''
