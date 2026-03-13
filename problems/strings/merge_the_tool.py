2c8cbccce7d43c77ddb9f589c04f1b94b8ae1ac6
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-13 12:33:01 +0530

    merge the tools problem

---
+++ problems/strings/merge_the_tool.py
@@ -0,0 +1,14 @@
+def merge_the_tools(string, k):
+    
+    for i in range(0, len(string), k):
+        sl = string[i : i+k]
+        result = ''
+        
+        for ch in sl:
+            if ch not in result:
+                result += ch
+        print(result)
+        
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
