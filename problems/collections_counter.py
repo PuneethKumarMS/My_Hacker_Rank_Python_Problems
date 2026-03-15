62acb952998e58f4191ac3398f0677441614cda3
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-15 14:15:29 +0530

    collections counter problem

---
+++ problems/collections_counter.py
@@ -0,0 +1,23 @@
+# Sell the shoe if the requested size is available and decrease its stock while adding the price to total earnings.
+
+from collections import Counter # Import Counter to count and track how many shoes are available for each size and it act as dictionary
+
+no_of_shoes = int(input())
+list_of_shoe_sizes = Counter(list(map(int, input().split())))
+no_of_customers = int(input())
+total = 0
+
+for i in range(no_of_customers):
+    size, price = map(int, input().split())
+    
+    if list_of_shoe_sizes[size] > 0:
+        total += price
+        list_of_shoe_sizes[size] -= 1
+print(total)  
+        
+    
+
+
+
+
+
