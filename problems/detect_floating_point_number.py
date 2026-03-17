687cca5aa3b1afe73b768ad0da39c1ce167b4321
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-17 14:15:55 +0530

    detect floating point number

---
+++ problems/detect_floating_point_number.py
@@ -0,0 +1,16 @@
+# print true if the number is float or else print false
+
+t = int(input())
+
+for i in range(t):
+    
+    n = input()
+    
+    if '.' in n:
+        try:
+            f = float(n)
+            print(True)
+        except:
+            print(False)
+    else:
+        print(False)
