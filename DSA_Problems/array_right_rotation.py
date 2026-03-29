ffb4ceca796d5755386bff67a5b58605513b5a10
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-29 12:15:24 +0530

    array right rotation problem added

---
+++ DSA_Problems/array_right_rotation.py
@@ -0,0 +1,13 @@
+def rotateRight(d, arr): 
+    n = len(arr)
+    d = d % n   
+    rotation = arr[-d:] + arr[:-d]
+    return rotation
+
+if __name__ == '__main__':
+
+    multiple_input = input().rstrip().split()
+    d = int(multiple_input[1])
+    arr = list(map(int, input().rstrip().split()))
+    result = rotateRight(d, arr)
+    print(*result)
