78ba59f4e0637edc9b92441f6a98c45e1e96d8bb
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-03-29 14:13:53 +0530

    string rotation problem added

---
+++ DSA_Problems/string_rotation.py
@@ -0,0 +1,15 @@
+def rotateString(d, string):
+    
+    r = string[d:] + string[:d]
+    return r
+
+if __name__ == '__main__':
+
+    multiple_input = input().split()
+    string = multiple_input[0]
+    d = int(multiple_input[1])
+    result = rotateString(d, string)
+    print(result)
+
+    
+
