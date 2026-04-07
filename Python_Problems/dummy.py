a86c27ca58658fb7e1a2b196b8258e85e054e657
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-04-07 21:45:03 +0530

    comments added to reverse num

--- Python_Problems/dummy.py
+++ Python_Problems/dummy.py
@@ -3,6 +3,9 @@ rev = 0
 
 while n > 0:
     digit = n % 10
+    #print(digit)
     rev = rev * 10 + digit
+    print(rev)
     n = n // 10
+    #print(n)
 print(rev)
--- Python_Problems/interview_prep/reverse_number.py
+++ Python_Problems/interview_prep/reverse_number.py
@@ -1,8 +1,13 @@
-n = int(input())
-rev = 0
+n = int(input())   # take input number
+rev = 0            # this will store the reversed number
 
-while n > 0:
-    digit = n % 10
-    rev = rev * 10 + digit
-    n = n // 10
-print(rev)
+while n > 0:       # run loop until all digits are removed
+    digit = n % 10         # extract last digit (e.g., 123 % 10 = 3)
+    
+    rev = rev * 10 + digit # shift rev left and add digit
+                           # example: rev=32 → 320 + 1 = 321
+    
+    n = n // 10            # remove last digit from n
+                           # example: 123 → 12
+
+print(rev)          # print final reversed number
