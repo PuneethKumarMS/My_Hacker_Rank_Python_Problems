b6952c4fb0d78a6c7bead95c2598984ee2a6fda4
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-04-10 12:54:09 +0530

    added logic to palindrome problem to handle edge cases

--- Python_Problems/interview_prep/palindrome.py
+++ Python_Problems/interview_prep/palindrome.py
@@ -1,16 +1,22 @@
 n = int(input()) 
 
-temp = n # to store number because number will modify after loop
-         # So we need to compare this stored number with reversed number
-
-rev = 0            
-
-while n > 0:       
-    digit = n % 10       
-    rev = rev * 10 + digit 
-    n = n // 10   
-
-if  rev == temp:
-    print('Palindrome')
-else:
+if n < 0:
     print('Not Palindrome')
+
+else:
+
+    temp = n # to store number because number will modify after loop
+            # So we need to compare this stored number with reversed number
+
+    rev = 0 
+
+    while n > 0:       
+        digit = n % 10       
+        rev = rev * 10 + digit 
+        n = n // 10   
+
+    if  rev == temp :
+        print('Palindrome')
+        
+    else:
+        print('Not Palindrome')
