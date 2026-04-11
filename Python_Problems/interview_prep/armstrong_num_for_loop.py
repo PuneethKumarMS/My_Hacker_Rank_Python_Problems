d0429b4d21ffc69476ab163a705e4a7a0fe21d5d
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-04-11 14:29:53 +0530

    armstrong number using for loop

---
+++ Python_Problems/interview_prep/armstrong_num_for_loop.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+if n < 0:
+    print("Not Armstrong")
+else:
+    s = str(n)              # convert number to string
+    power = len(s)          # number of digits
+
+    total = 0
+
+    for digit in s:         # iterate through each character
+        total += int(digit) ** power
+
+    print("Armstrong" if total == n else "Not Armstrong")
