ab5041f8a06f302f684abb098fac3a61d395bcf0
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-04-28 13:33:19 +0530

    find duplicate num using hashing

---
+++ Python_Problems/interview_prep/find_duplicate_num2.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = list(map(int, input().split()))
+
+seen = []
+
+for num in arr:
+
+    if num in seen:
+        print(num)
+        break
+
+    seen.append(num)
+
+else:
+    print(-1)
