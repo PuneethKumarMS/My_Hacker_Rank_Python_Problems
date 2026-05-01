73077cd0cdc786383418d29b029617fac35885aa
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-05-01 15:32:50 +0530

    find an occurence of each element inarray

---
+++ Python_Problems/interview_prep/frequency_of_element.py
@@ -0,0 +1,19 @@
+arr = list(map(int, input().split()))
+
+visited = []
+
+for i in arr:
+
+   if i in visited:
+      continue
+   
+   count = 0
+
+   for j in arr:
+      
+      if i == j:
+         count += 1
+
+   print(i, '->', count)
+
+   visited.append(i)
