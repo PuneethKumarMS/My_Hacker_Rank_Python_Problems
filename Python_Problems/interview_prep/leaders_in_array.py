a634a310dfd276c9bc60d86b2137f68a1126b76e
PuneethKumar M S <puneethkumarms912@gmail.com>
2026-05-06 14:19:58 +0530

    comment

--- Python_Problems/interview_prep/leaders_in_array.py
+++ Python_Problems/interview_prep/leaders_in_array.py
@@ -10,7 +10,7 @@ max_from_right = arr[-1]
 
 for i in range(n-1, -1, -1):
     if arr[i] >=  max_from_right:  # last element iteslf is leader
-        leaders.append(arr[i])
+        leaders.append(arr[i])  # after comparing added to the new list
         max_from_right = arr[i] # arr[i] is stored to max after comparing
     
 print(leaders[::-1])
